c799775056429f9804809642782e764d87f479a7
SiciliaLee <xslialee070@outlook.com>
2021-02-24 14:34:24 +0800

    Update index.py

--- hw#2/index.py
+++ hw#2/index.py
@@ -7,18 +7,74 @@ import sys
 import getopt
 import os
 
+punctuation = ['"', ',', '&', '(', ')', '-', '.', "'"]
+M = 20 #### set memory size, you can personalize ####
+
 def usage():
     print("usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file")
 
-def data_preprocess(file):
+class term_dict(object):
+    def __init__(self):
+        '''
+        initialize the params
+        '''
+        self.terms = dict() # format: "term":[doc_freq, postings_list]
+        self.size = 0 # size of occupied memory
+        self.tmpidx = 0 # indexing output 'blk_X.txt'
+
+    def add_terms(self, term, doc_id):
+        '''
+        add terms to the dictionary
+            --> if available memory: store in mem
+            ---> if not available: write to disk
+        :param term:
+        :param doc_id:
+        :return:
+        '''
+        if self.size < M - 1: # still have space to store terms
+            if self.terms.__contains__(term):
+                if self.terms[term][1][-1] != doc_id: # current term has occured in the postings
+                    self.terms[term][1].append(doc_id)
+                    self.terms[term][0] += 1
+            else:
+                postings = [doc_id]
+                self.terms[term] = [1, postings]
+                self.size += 1
+
+        else: # no enough space, write current 'self.terms' to blkX.txt
+            tmpfile = "blk" + str(self.tmpidx) + ".txt" # generated file name #### you can chagne the index ####
+            #TODO: write self.terms to blkx.txt
+            f = open(tmpfile, 'w')
+            for term in sorted(self.terms):
+                write_info =  term + " " + str(self.terms[term][0])
+                for posting in self.terms[term][1]:
+                    write_info += " " + str(posting)
+                f.write(write_info+"\n")
+            # write format: term doc_freq docID1 docID2 .....\n
+            self.size = 0
+            self.terms = dict()
+            self.tmpidx += 1
+
+
+def data_preprocess(line):
     '''
     tokenize, porter stemmer,
-    :param file:
-    :return:
+    :param file: file name
+    :return: list of processed terms
     '''
     stemmer = PorterStemmer()
-    with open(file, 'rb') as f:
-        sentence = file.read()
+    return_term = []
+    words = nltk.sent_tokenize(line)
+    for word in words:
+        tokens = nltk.word_tokenize(word)
+            # print(tokens)
+        for token in tokens:
+            token = stemmer.stem(token).lower()
+            if token not in punctuation:
+                return_term.append(token)
+
+    return sorted(return_term)
+
 
 def build_index(in_dir, out_dict, out_postings):
     """
@@ -30,12 +86,23 @@ def build_index(in_dir, out_dict, out_postings):
     """
     print('indexing...')
     path = os.listdir(in_dir)
-    reuter_files = sorted([file for file in path], key=int) #filename list
-    reuter_files = reuter_files[:100] # remember to delete this
-
+    reuter_files = sorted([file for file in path], key=int)  # filename list, sorted
+    reuter_files = reuter_files[:2]  # remember to delete this
+    dictionary = term_dict()
     for file in reuter_files:
-        with open(file) as f:
+        f = open(in_dir + file)
+        while True:
+            line = f.readline() # read line by line
+            if not line: # finish reading, end
+                break
+            else:
+                tokens = data_preprocess(line)
+                if tokens == []: # if null, continue to process
+                    continue
+                for token in tokens:
+                    dictionary.add_terms(token, int(file))
 
+        f.close()
 
 
 
